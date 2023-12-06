908de7bee3e13da4ebcd2d9adf9336ab513f1d2d
Jimmy <j3jimmy@gmail.com>
2023-12-05 23:53:02 -0500

    advances

--- assets/js/form.js
+++ assets/js/form.js
@@ -14,4 +14,34 @@ document.addEventListener('DOMContentLoaded', function () {
   closeButton.addEventListener("click", () => {
     dialog.close();
   });
+
+
+
+
+  //form -emailJS
+
+  const btnSend = document.getElementById('btn-send');
+  const form = document.getElementById('form')
+
+  btnSend.addEventListener('click', function (e) {
+    e.preventDefault();
+
+
+    const serviceID = 'service_7hh0jlg';
+    const templateID = 'template_jtsq46o';
+
+    emailjs.sendForm(serviceID, templateID, form)
+      .then(() => {
+        window.alert('Sent!');
+        form.reset();
+        dialog.close();
+
+      })
+      .catch((error) => {
+        alert(JSON.stringify(error));
+      })
+
+
+
+  })
 })
